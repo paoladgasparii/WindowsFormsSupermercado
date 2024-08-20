1ecaff3933d13584f0ceaab59e25a851b3e2aeed
Usuario <Usuario@CPD03-M03>
2024-08-20 10:51:31 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsSupermercado/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsSupermercado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.txtQtde = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.lstProdutos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AccessibleName = "";
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(84, 52);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(57, 16);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Produto:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AccessibleName = "";
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(61, 93);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(80, 16);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Quantidade:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AccessibleName = "";
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(50, 137);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(91, 16);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Valor Unitário:";
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.AccessibleName = "";
+            this.txtProduto.Location = new System.Drawing.Point(169, 51);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(270, 20);
+            this.txtProduto.TabIndex = 3;
+            // 
+            // txtQtde
+            // 
+            this.txtQtde.AccessibleName = "";
+            this.txtQtde.Location = new System.Drawing.Point(169, 92);
+            this.txtQtde.Name = "txtQtde";
+            this.txtQtde.Size = new System.Drawing.Size(270, 20);
+            this.txtQtde.TabIndex = 4;
+            // 
+            // txtValor
+            // 
+            this.txtValor.AccessibleName = "";
+            this.txtValor.Location = new System.Drawing.Point(169, 136);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(270, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.AccessibleName = "";
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(169, 195);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(151, 53);
+            this.btnInserir.TabIndex = 6;
+            this.btnInserir.Text = "Inserir Produto";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // lstProdutos
+            // 
+            this.lstProdutos.AccessibleName = "";
+            this.lstProdutos.FormattingEnabled = true;
+            this.lstProdutos.Location = new System.Drawing.Point(64, 277);
+            this.lstProdutos.Name = "lstProdutos";
+            this.lstProdutos.Size = new System.Drawing.Size(375, 290);
+            this.lstProdutos.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 619);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Total da venda:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(184, 619);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 16);
+            this.lblTotal.TabIndex = 9;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(87, 665);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(140, 32);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Nova venda";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(279, 665);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(140, 32);
+            this.btnFechar.TabIndex = 11;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 737);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstProdutos);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtQtde);
+            this.Controls.Add(this.txtProduto);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.TextBox txtQtde;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.ListBox lstProdutos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ WindowsFormsSupermercado/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSupermercado
+{
+    public partial class Form1 : Form
+    {
+        double total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            // Entrada 
+            double qtde = double.Parse(txtQtde.Text);
+            double valor = double.Parse(txtValor.Text);
+
+            // Procesamento
+            total += qtde * valor;
+
+            // Saída 
+            lblTotal.Text = total.ToString("C");
+
+            // Insere o produto no ListBox
+            lstProdutos.Items.Add(txtProduto.Text + " / " + txtQtde.Text + " / " + valor.ToString("C"));
+
+            // Limpa as caixas para inserir novo produto
+            txtProduto.Clear();
+            txtQtde.Clear();
+            txtValor.Clear();
+            txtProduto.Focus();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Zera a variavel total 
+            total = 0;
+            lblTotal.Text = total.ToString("C");
+
+            // Limpa o ListBox
+            lstProdutos.Items.Clear();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            // Encerra a aplicação
+            Application.Exit();
+        }
+    }
+}
